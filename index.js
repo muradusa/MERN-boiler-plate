b48928c9a403df70c2c53b6969a9bad790a1f13c
Murad Cholukov <muradusa@gmail.com>
2020-05-16 11:35:58 -0700

    move private info into dev.js

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
-
 const mongoose = require("mongoose");
-mongoose.connect('mongodb+srv://muradusa:12345@mern-boiler-at9rc.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true})
-.then(() => console.log('DB connected'))
-.catch(err => console.error(err));
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-app.get('/', (req, res) => {
-    res.send("hello world")
-})
+const config = require("./config/key");
+
+const { User } = require("./models/user");
+
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+  });
+  return res.status(200).json({
+    success: true,
+  });
+});
 
 app.listen(5000);
