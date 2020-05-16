5dd530ac609a4b53034c76e742e51037b133b1a1
Murad Cholukov <muradusa@gmail.com>
2020-05-16 10:11:19 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const app = express();
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb+srv://muradusa:12345@mern-boiler-at9rc.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true})
+.then(() => console.log('DB connected'))
+.catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
+
+app.listen(5000);
