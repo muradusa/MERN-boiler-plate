ffa7b7fc2504ed699e7615909307bb29d6566844
Murad Cholukov <muradusa@gmail.com>
2020-05-17 12:17:35 -0700

    initialize heroku deploymnet

--- server/index.js
+++ server/index.js
@@ -73,4 +73,18 @@ app.post("/api/users/login", (req, res) => {
   });
 });
 
-app.listen(5000);
+app.get("/api/user/logout", auth, (req, res) => {
+  User.findOneAndUpdate({_id: req.user._id}, { token: ""}, (err, doc) =>{
+    if(err) return res.json({ success: false, err })
+    return res.status(200).send({
+      success: true
+    })
+
+  })
+})
+
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+});
