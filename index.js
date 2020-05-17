f388b46cb0c54bfa4ade61284f704bc456091f1f
Murad Cholukov <muradusa@gmail.com>
2020-05-17 11:09:21 -0700

    Finished with setting up authorization tokens

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
 const { User } = require("./models/user");
+const { auth } = require("./middleware/auth");
+
 
 mongoose
   .connect(config.mongoURI, { useNewUrlParser: true })
@@ -21,6 +23,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get("/api/user/auth", auth, (req, res) => {
+
+  res.status(200).json({
+    _id: req._id,
+    isAuth: true,
+    email:req.user.email,
+    name:req.user.name,
+    lastname:req.user.lastname,
+    role:req.user.role
+  })
+
+})
+
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
 
@@ -29,6 +44,32 @@ app.post("/api/users/register", (req, res) => {
   });
   return res.status(200).json({
     success: true,
+  });
+});
+
+app.post("/api/users/login", (req, res) => {
+  //find email
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found"
+      });
+
+    //Compare Password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ loginSuccess: false, message: "wrong password" });
+      }
+    });
+
+    //Generate Token
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res.cookie("x_auth", user.token).status(200).json({
+        loginSuccess: true,
+      });
+    });
   });
 });
 
